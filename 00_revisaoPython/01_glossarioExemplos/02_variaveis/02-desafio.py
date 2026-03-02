f3ee7e4bf6be974f688533b4f625d03215778daf
Maísa Bom <20251ctb0100037@estudantes.ifpr.edu.br>
2026-03-02 17:45:19 +0000

    emprestaIFPR.py

--- 00_revisaoPython/01_glossarioExemplos/02_variaveis/02-desafio.py
+++ 00_revisaoPython/01_glossarioExemplos/02_variaveis/02-desafio.py
@@ -1,49 +1,78 @@
 # ===================================================================
-# SISTEMA DE APROVAÇÃO DE ALUNOS                                    =
+# SISTEMA DE APROVAÇÃO DE ALUNOS
 # ===================================================================
-# DISCIPLINA : Programação de Sistemas (PS)                         =
-# AULA       : 04 - Revisão: Variáveis, Tipos e Controle de Fluxo   =
-# NOME       : Maísa Gabriele Bom                                   =
-# DATA       : 24/02/2026                                           =
+# DISCIPLINA : Programação de Sistemas (PS)
+# AULA       : 04 - Atividade de desafio
+# NOME       : Maísa Gabriele Bom
+# DATA       : 24/02/2026
 # ===================================================================
-# REPOSITÓRIO: https://github.com/20251ctb0100037-code/2026-PS      =
+# REPOSITÓRIO: https://github.com/20251ctb0100037-code/2026-PS      
 # ===================================================================
-# DESCRIÇÃO  :                                                      =
-# Este programa determina a situação de cada aluno com base nas     =
-# notas deles ( se o aluno será Aprovado, esta e recuperação ou foi =
-# Reprovado).                                                       =
+# DESCRIÇÃO  :
+# Este sistema mostra o controle de estoque de uma loja de
+# informática.
 # ===================================================================
-# CONCEITOS  : variáveis, tipos de dados, operadores, estruturas de =
-# seleção e de reétição                                             =
+# CONCEITOS  : variáveis, tipos de dados, operadores, estruturas de
+# seleção e de repetição.
 # ===================================================================
-#                                                                   =
-# ENTRADA DE DADOS ==================================================
-print("=============================================================")
-print("=========== * SISTEMA DE APROVAÇÃO DE ALUNOS * ==============")
-print("=============================================================")
-print("=============== * DIGITE O NOME DO ALUNO * ==================")
-nome = input(" ") #string para entrada de texto
-print("=============================================================")
-print("========== * DIGITE A NOTA 1 DO ALUNO (0 a 10) * ============")
-nota1 = float(input("")) # str para a entrada de um número
-print("=============================================================")
-print("========== * DIGITE A NOTA 2 DO ALUNO (0 a 10) * ============")
-nota2 = float(input(""))
-print("=============================================================")
-print("========== * DIGITE A NOTA 3 DO ALUNO (0 a 10) * ============")
-nota3 = float(input(""))
-#
-media = (nota1 + nota2 + nota3) / 3 # calula a media do aluno 
-if media >= 6.0: #compara o resultado com a situação
-    situacao = "Aprovado"
-elif media >= 4.0:
-    situacao = "Recuperacao"
-else: 
-    situacao = "Reprovado"
-#
-print("=============================================================")
-print(f"========== * O ALUNO: {nome} TEM ESTA MÉDIA {media:.2f} * ==========")
-print("=============================================================")
-print("=============================================================")
-print(f"============ * E ESTÁ NESTA SITUAÇÃO: {situacao} * ============")
-print("=============-===============================================")
+# apresentação organizada do programa
+print("=== SISTEMA DE CONTROLE DE ESTOQUE ===")
+print()
+# lista de produtos no estoque
+produtos = [
+    {"nome": "Notebook", "qtd": 7},
+    {"nome": "Teclado", "qtd": 2},
+    {"nome": "Pendrive", "qtd": 30}
+]
+
+# Conta a situaçao dos estoques
+critico = 0
+adequado = 0
+excesso = 0
+
+print("=== RELATÓRIO DE ESTOQUE ===")
+
+# Laço FOR para ler todos os produtos
+for produto in produtos:
+    nome = produto["nome"]
+    qtd = produto["qtd"]
+
+    # Verifica a situação do estoque
+    if qtd < 5:
+        situacao = "Crítico"
+        critico += 1
+    elif qtd <= 20:
+        situacao = "Adequado"
+        adequado += 1
+    else:
+        situacao = "Excesso"
+        excesso += 1
+
+    # Mostra as informações do produto
+    print(f"Produto: {nome} | Quantidade: {qtd} | Situação: {situacao}")
+
+# mostra o estoque em geral
+print("\n=== RESUMO DO ESTOQUE ===")
+print("Produtos Críticos:", critico)
+print("Produtos Adequados:", adequado)
+print("Produtos em Excesso:", excesso)
+
+# Laço WHILE para encontrar o produto pelo nome
+while True:
+    busca = input("\nDigite o nome do produto para consultar (ou 'sair' para encerrar): ")
+
+    if busca.lower() == "sair":
+        print("=== PROGRAMA FINALIZADO! ===")
+        break
+
+    encontrado = False
+
+    # Busca o produto na lista
+    for produto in produtos:
+        if produto["nome"].lower() == busca.lower():
+            print(f"{produto['nome']} tem {produto['qtd']} unidades em estoque.")
+            encontrado = True
+
+    # se o produto for falso
+    if not encontrado:
+        print("Produto não encontrado na lista! ")
